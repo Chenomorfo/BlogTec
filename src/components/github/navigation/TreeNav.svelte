e978e18ea951197ad1b29377981e0ae49498f3a3
Chenomorfo <angel_humberto_1502@hotmail.com>
2024-05-29 12:24:36 -0500

    Svelte Configurado para usar en el apartado de GIT, junto con su componente de Navegacion

---
+++ src/components/github/navigation/TreeNav.svelte
@@ -0,0 +1,41 @@
+<nav>
+    <ul>
+        <li>
+            <details>
+                <summary>Que es?</summary>
+                <ul>
+                    <li>Introduccion</li>
+                    <li>Donde descargar</li>
+                    <li>Guia rapida</li>
+                    <li>El creador de...</li>
+                    <li>Utilidad</li>
+                    <li>Test</li>
+                </ul>
+            </details>
+        </li>
+        <li>
+            <details>
+                <summary>CLI</summary>
+                <li>Resumen de</li>
+                <li>Comandos basicos</li>
+                <li>Comandos avanzados</li>
+                <li>Commits</li>
+                <li>Logs</li>
+                <li>Branches</li>
+                <li>Test</li>
+            </details>
+        </li>
+        <li>wasap</li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background-color: #575555;
+        padding-left: 12px;
+        padding-top: 8px;
+        padding-bottom: 20px;
+        height: 100%;
+        width: 220px;
+    }
+</style>
